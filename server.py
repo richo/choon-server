fde2ff9b48e4907c99f7a41b2853ed4e6e559a20
Richo Healey <richo@psych0tik.net>
2013-06-16 23:25:53 +1000

    basic request routing machinery

--- server.py
+++ server.py
@@ -4,6 +4,46 @@ import os
 import sys
 import socket
 import logging
+import select
+
+from collections import defaultdict
+
+peers = []
+register = defaultdict(list)
+
+
+def terminate(conn):
+    conn.shutdown(socket.SHUT_WR)
+    conn.close()
+    peers.pop(peers.index(conn))
+
+
+def handle_register(data, conn):
+    pebble_id = data.split(" ")[1]
+    register[pebble_id].append(conn)
+    logging.warn("Registered %s" % pebble_id)
+
+
+def handle_http(data):
+    try:
+        verb, path, http = data.split(" ")
+        _, pebble_id, action = path.split("/")
+        byte = action[0]
+        for conn in register[pebble_id]:
+            conn.send(byte)
+    except:
+        raise
+
+
+def handle_incoming(data, conn):
+    if data.startswith("reg "):
+        handle_register(data, conn)
+    elif data.startswith("POST /"):
+        handle_http(data)
+        terminate(conn)
+    else:
+        terminate(conn)
+
 
 def main(argv):
     if len(argv) > 1:
@@ -16,11 +56,19 @@ def main(argv):
     sock.bind(("127.0.0.1", port))
     logging.info("Bound socket on port: %i" % (port))
     sock.listen(16)
-    conn, peer = sock.accept()
-    logging.info("Accepted connection from: %s:%i" % peer)
-    logging.warn("Dropping into debugger")
-    import pdb; pdb.set_trace()
 
+    while True:
+        r_socks = peers + [sock]
+        r, _, _ = select.select(r_socks, [], [])
+        for i in r:
+            if i == sock:
+                conn, peer = sock.accept()
+                logging.info("Accepted connection from: %s:%i" % peer)
+                peers.append(conn)
+            else:
+                data = i.recv(1024)
+                print ">>> %s" % data
+                handle_incoming(data, i)
 
 if __name__ == "__main__":
     main(sys.argv)
